ec86a577aebf62d75250b1bc66ee3ed0e82724e1
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-04 21:59:52 +0530

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,32 @@
+import { LoaderFunction, useLoaderData } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = api.json();
+  return res;
+};
+
+export default function Index() {
+  const data = useLoaderData();
+  console.log(data);
+  return (
+    <div className="bg-black min-h-screen text-white/80">
+      <h1 className="text-center text-6xl py-10 ">Remix Basic Data fetching </h1>
+      <div className="grid justify-center flex-col items-center  ">
+        {data.map(
+          (n: { body: string; title: string; id: number }, index: number) => {
+            return (
+              <div
+                key={index}
+                className="bg-slate-700 my-3 p-5 rounded-2xl max-w-3xl"
+              >
+                <h1 className="text-3xl font-mono ">{n.title}</h1>
+                <p className="text-xl">{n.body}</p>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+}
